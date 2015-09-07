ec88d676fa561370cbd50365abf31370b048e539
Seehait C <kiseehait@gmail.com>
2015-09-07 19:37:06 +0700

    Add write image to file feature. use argument -ss [width] [height]
    output file name: capture.bmp
    output file location: project root

--- Task1_Glut_Circle/main.cpp
+++ Task1_Glut_Circle/main.cpp
@@ -80,6 +80,9 @@ vector<Light> lights;
 Viewport	viewport;
 int 		drawX = 0;
 int 		drawY = 0;
+int         captureWidth = 0;
+int         captureHeight = 0;
+bool        isCapture = false;
 
 
 void initScene(){
@@ -159,8 +162,166 @@ vec3 computeShadedColor(vec3 pos) {
         vec3 spc = vec3(material.ks.r*lc.r,material.ks.g*lc.g,material.ks.b*lc.b)*pow(max(r*v,0.0f),material.sp);
         sum += amb + dif + spc;
     }
-    sum = toonShading(sum,pos);
+    // 5sum = toonShading(sum,pos);
     return  sum;
+}
+
+//****************************************************
+// compute the bmp map vector[y position][x position][0 = red, 1 = green, 2 = blue]
+//****************************************************
+vector<vector<vector<float>>> computeBitMap(int iwidth, int iheight) {
+    vector<vector<vector<float>>> bmap;
+    bmap.resize(iheight);
+    for (int i = 0; i < iheight; i++) {
+        bmap[i].resize(iwidth);
+        for (int j = 0; j < iwidth; j++) {
+            bmap[i][j].resize(3);
+            bmap[i][j][0] = 1.0f;
+            bmap[i][j][1] = 1.0f;
+            bmap[i][j][2] = 1.0f;
+        }
+    }
+
+    int drawRadius = min(iwidth, iheight)/2 - 10;  // Make it almost fit the entire window
+	float idrawRadius = 1.0f / drawRadius;
+
+
+
+	for (int i = -drawRadius; i <= drawRadius; i++) {
+		int width = floor(sqrt((float)(drawRadius*drawRadius-i*i)));
+		for (int j = -width; j <= width; j++) {
+
+			// Calculate the x, y, z of the surface of the sphere
+			float x = j * idrawRadius;
+			float y = i * idrawRadius;
+			float z = sqrtf(1.0f - x*x - y*y);
+			vec3 pos(x,y,z); // Position on the surface of the sphere
+
+			vec3 col = computeShadedColor(pos);
+
+			// Set the red pixel
+			bmap[j + iheight/2][i + iwidth/2][0] = col.r;
+			bmap[j + iheight/2][i + iwidth/2][1] = col.g;
+			bmap[j + iheight/2][i + iwidth/2][2] = col.b;
+		}
+	}
+    return bmap;
+}
+
+//****************************************************
+// Write BMP to file
+//****************************************************
+void drawbmp(char* filename, int WIDTH, int HEIGHT) {
+
+    unsigned int headers[13];
+    FILE * outfile;
+    int extrabytes;
+    int paddedsize;
+    int x; int y; int n;
+    int red, green, blue;
+
+    vector<vector<vector<float>>> bmap = computeBitMap(WIDTH, HEIGHT);
+
+    printf("BMP calculated\n");
+
+    extrabytes = 4 - ((WIDTH * 3) % 4);                 // How many bytes of padding to add to each
+                                                        // horizontal line - the size of which must
+                                                        // be a multiple of 4 bytes.
+    if (extrabytes == 4)
+       extrabytes = 0;
+
+    paddedsize = ((WIDTH * 3) + extrabytes) * HEIGHT;
+
+    // Headers...
+    // Note that the "BM" identifier in bytes 0 and 1 is NOT included in these "headers".
+
+    headers[0]  = paddedsize + 54;      // bfSize (whole file size)
+    headers[1]  = 0;                    // bfReserved (both)
+    headers[2]  = 54;                   // bfOffbits
+    headers[3]  = 40;                   // biSize
+    headers[4]  = WIDTH;  // biWidth
+    headers[5]  = HEIGHT; // biHeight
+
+    // Would have biPlanes and biBitCount in position 6, but they're shorts.
+    // It's easier to write them out separately (see below) than pretend
+    // they're a single int, especially with endian issues...
+
+    headers[7]  = 0;                    // biCompression
+    headers[8]  = paddedsize;           // biSizeImage
+    headers[9]  = 0;                    // biXPelsPerMeter
+    headers[10] = 0;                    // biYPelsPerMeter
+    headers[11] = 0;                    // biClrUsed
+    headers[12] = 0;                    // biClrImportant
+
+    outfile = fopen(filename, "wb");
+
+    //
+    // Headers begin...
+    // When printing ints and shorts, we write out 1 character at a time to avoid endian issues.
+    //
+
+    fprintf(outfile, "BM");
+
+    for (n = 0; n <= 5; n++)
+    {
+       fprintf(outfile, "%c", headers[n] & 0x000000FF);
+       fprintf(outfile, "%c", (headers[n] & 0x0000FF00) >> 8);
+       fprintf(outfile, "%c", (headers[n] & 0x00FF0000) >> 16);
+       fprintf(outfile, "%c", (headers[n] & (unsigned int) 0xFF000000) >> 24);
+    }
+
+    // These next 4 characters are for the biPlanes and biBitCount fields.
+
+    fprintf(outfile, "%c", 1);
+    fprintf(outfile, "%c", 0);
+    fprintf(outfile, "%c", 24);
+    fprintf(outfile, "%c", 0);
+
+    for (n = 7; n <= 12; n++)
+    {
+       fprintf(outfile, "%c", headers[n] & 0x000000FF);
+       fprintf(outfile, "%c", (headers[n] & 0x0000FF00) >> 8);
+       fprintf(outfile, "%c", (headers[n] & 0x00FF0000) >> 16);
+       fprintf(outfile, "%c", (headers[n] & (unsigned int) 0xFF000000) >> 24);
+    }
+
+    //
+    // Headers done, now write the data...
+    //
+
+    for (y = HEIGHT - 1; y >= 0; y--)     // BMP image format is written from bottom to top...
+    {
+       for (x = 0; x <= WIDTH - 1; x++)
+       {
+
+          red = bmap[x][WIDTH - y - 1][0] * 255;
+          green = bmap[x][WIDTH - y - 1][1] * 255;
+          blue = bmap[x][WIDTH - y - 1][2] * 255;
+
+          if (red > 255) red = 255; if (red < 0) red = 0;
+          if (green > 255) green = 255; if (green < 0) green = 0;
+          if (blue > 255) blue = 255; if (blue < 0) blue = 0;
+
+          // Also, it's written in (b,g,r) format...
+
+          fprintf(outfile, "%c", blue);
+          fprintf(outfile, "%c", green);
+          fprintf(outfile, "%c", red);
+       }
+       if (extrabytes)      // See above - BMP lines must be of lengths divisible by 4.
+       {
+          for (n = 1; n <= extrabytes; n++)
+          {
+             fprintf(outfile, "%c", 0);
+          }
+       }
+    }
+
+    fclose(outfile);
+
+    printf("BMP file: %s created!\n", filename);
+
+    return;
 }
 //****************************************************
 // function that does the actual drawing of stuff
@@ -195,7 +356,6 @@ void myDisplay() {
 		}
 	}
 	glEnd();
-
 	glFlush();
 	glutSwapBuffers();					// swap buffers (we earlier set double buffer)
 
@@ -272,6 +432,12 @@ void parseArguments(int argc, char* argv[]) {
 			}
 			lights.push_back(light);
 			i+=7;
+		} else
+		if (strcmp(argv[i], "-ss") == 0){
+			captureWidth = atoi(argv[i+1]);
+			captureHeight = atoi(argv[i+2]);
+			isCapture = true;
+			i+=3;
 		}
 	}
 }
@@ -282,6 +448,11 @@ void parseArguments(int argc, char* argv[]) {
 int main(int argc, char *argv[]) {
 
 	parseArguments(argc, argv);
+
+	if (isCapture) {
+        drawbmp("capture.bmp", captureWidth, captureHeight);
+        return 0;
+	}
 
   	//This initializes glut
   	glutInit(&argc, argv);
