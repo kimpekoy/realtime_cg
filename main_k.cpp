0a3769fafa3fe74f46ea106a9d603b6d5b0e4dd7
kimpekoy <kimpekoy@hotmail.com>
2015-09-08 06:02:34 +0700

    Update main_k.cpp

--- main_k.cpp
+++ main_k.cpp
@@ -1,1 +1,335 @@
+// Simple OpenGL example for CS184 F06 by Nuttapong Chentanez, modified from sample code for CS184 on Sp06
+// Modified for Realtime-CG class
+
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+#define _WIN32
+
+#ifdef _WIN32
+#	include <windows.h>
+#else
+#	include <sys/time.h>
+#endif
+
+#ifdef OSX
+#include <GLUT/glut.h>
+#include <OpenGL/glu.h>
+#else
+#include <GL/glut.h>
+#include <GL/glu.h>
+#endif
+
+#include <time.h>
+#include <math.h>
+#include "algebra3.h"
+
+#ifdef _WIN32
+static DWORD lastTime;
+#else
+static struct timeval lastTime;
+#endif
+
+#define PI 3.14159265
+
+
+
+using namespace std;
+
+//****************************************************
+// Some Classes
+//****************************************************
+
+class Viewport;
+
+class Viewport {
+public:
+	int w, h; // width and height
+};
+
+class Material{
+public:
+	vec3 ka; // Ambient color
+	vec3 kd; // Diffuse color
+	vec3 ks; // Specular color
+	float sp; // Power coefficient of specular
+
+	Material() : ka(0.0f), kd(0.0f), ks(0.0f), sp(0.0f) {
+	}
+};
+
+class Light{
+public:
+	enum LIGHT_TYPE{POINT_LIGHT, DIRECTIONAL_LIGHT};
+
+	vec3 posDir;  // Position (Point light) or Direction (Directional light)
+	vec3 color;   // Color of the light
+	LIGHT_TYPE type;
+
+	Light() : posDir(0.0f), color(0.0f), type(POINT_LIGHT) {
+	}
+};
+
+// Material and lights
+Material material;
+vector<Light> lights;
+
+//****************************************************
+// Global Variables
+//****************************************************
+Viewport	viewport;
+int 		drawX = 0;
+int 		drawY = 0;
+
+void initScene(){
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // Clear to black, fully transparent
+
+	glViewport (0,0,viewport.w,viewport.h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0,viewport.w, 0, viewport.h);
+}
+
+
+//****************************************************
+// reshape viewport if the window is resized
+//****************************************************
+void myReshape(int w, int h) {
+	viewport.w = w;
+	viewport.h = h;
+
+	glViewport (0,0,viewport.w,viewport.h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0, viewport.w, 0, viewport.h);
+
+	drawX = (int)(viewport.w*0.5f);
+	drawY = (int)(viewport.h*0.5f);
+
+
+}
+
+void setPixel(int x, int y, GLfloat r, GLfloat g, GLfloat b) {
+	glColor3f(r, g, b);
+	glVertex2f(x+0.5, y+0.5);
+}
+vec3 diffuse(vec3 pos)
+{  //   float r,g,b;
+      vec3 sum(0.0f,0.0f,0.0f);
+      vec3 n = pos.normalize();
+      for(int i = 0;i<lights.size();i++){
+             vec3 l = lights[i].type == Light::DIRECTIONAL_LIGHT ? lights[i].posDir.normalize() : (lights[i].posDir - pos).normalize();
+             sum.r+= max(l*n,0.0f) * lights[i].color.r*material.kd.r;
+             sum.g+= max(l*n,0.0f) * lights[i].color.g*material.kd.g;
+             sum.b+= max(l*n,0.0f) * lights[i].color.b*material.kd.b;
+
+      }
+      return sum;
+
+
+}
+vec3 specular(vec3 pos){
+
+       vec3 n = pos.normalize();
+       vec3 sum(0.0f,0.0f,0.0f);
+       vec3 v(0.0f,0.0f,1.0f);
+       for(int i = 0;i<lights.size();i++){
+               vec3 l = lights[i].type == Light::DIRECTIONAL_LIGHT ? lights[i].posDir.normalize() : (lights[i].posDir - pos).normalize();
+               vec3 r = (2.0*(l*n))*n-(l);
+               r = r.normalize();
+
+               sum.r+=material.ks.r*lights[i].color.r*pow(max(r*v,0.0f),material.sp);
+               sum.g+=material.ks.g*lights[i].color.g*pow(max(r*v,0.0f),material.sp);
+               sum.b+=material.ks.b*lights[i].color.b*pow(max(r*v,0.0f),material.sp);
+
+       }
+    return sum;
+
+}
+
+vec3 ambient(){
+      vec3 sum(0.0f,0.0f,0.0f);
+       for(int i = 0;i<lights.size();i++){
+         sum.r += material.ka.r*lights[i].color.r;
+         sum.g += material.ka.g*lights[i].color.g;
+         sum.b += material.ka.b*lights[i].color.b;
+       }
+      return sum;
+}
+vec3 computeShadedColor(vec3 pos) {
+
+	// TODO: Your shading code mostly go here
+
+	return  diffuse(pos)+ambient()+specular(pos);
+	//return  diffuse(pos);
+	//return  ambient();
+	//return specular(pos);
+}
+
+
+//******************************** ********************
+// function that does the actual drawing of stuff
+//***************************************************
+double rad;
+void myDisplay() {
+
+	glClear(GL_COLOR_BUFFER_BIT);				// clear the color buffer
+
+	glMatrixMode(GL_MODELVIEW);					// indicate we are specifying camera transformations
+	glLoadIdentity();							// make sure transformation is "zero'd"
+
+
+	int h = min(viewport.w, viewport.h)/2 - 10;  // Make it almost fit the entire window
+	float ih = 1.0f / h;
+	// Start drawing sphere
+	glBegin(GL_POINTS);
+
+	for (int i = -h; i <= h; i++) {
+		int width = h-((i+h)/2);
+		for (int j = -width; j <= width; j++) {
+
+			// Calculate the x, y, z of the surface of the sphere
+			float x = j * ih;
+			float y = i * ih;
+			float z = sqrtf((width*ih*width*ih) - x*x);
+			vec3 pos(x,y,z); // Position on the surface of the sphere
+
+			vec3 col = computeShadedColor(pos);
+
+			// Set the red pixel
+			setPixel(drawX + j, drawY + i, col.r, col.g, col.b);
+		}
+	}
+
+	glEnd();
+
+	glFlush();
+	glutSwapBuffers();
+//	for(int i = 0;i<lights.size();i++)
+	     lights[0].posDir =vec3(50,0,20);					// swap buffers (we earlier set double buffer)
+}
+
+
+//****************************************************
+// for updating the position of the circle
+//****************************************************
+
+void myFrameMove() {
+	float dt;
+	// Compute the time elapsed since the last time the scence is redrawn
+#ifdef _WIN32
+	DWORD currentTime = GetTickCount();
+	dt = (float)(currentTime - lastTime)*0.001f;
+#else
+	timeval currentTime;
+	gettimeofday(&currentTime, NULL);
+	dt = (float)((currentTime.tv_sec - lastTime.tv_sec) + 1e-6*(currentTime.tv_usec - lastTime.tv_usec));
+#endif
+
+	// Store the time
+	lastTime = currentTime;
+	glutPostRedisplay();
+}
+
+
+void parseArguments(int argc, char* argv[]) {
+	int i = 1;
+	while (i < argc) {
+		if (strcmp(argv[i], "-ka") == 0) {
+			// Ambient color
+			material.ka.r = (float)atof(argv[i+1]);
+			material.ka.g = (float)atof(argv[i+2]);
+			material.ka.b = (float)atof(argv[i+3]);
+			i+=4;
+		} else
+		if (strcmp(argv[i], "-kd") == 0) {
+			// Diffuse color
+			material.kd.r = (float)atof(argv[i+1]);
+			material.kd.g = (float)atof(argv[i+2]);
+			material.kd.b = (float)atof(argv[i+3]);
+			i+=4;
+		} else
+		if (strcmp(argv[i], "-ks") == 0) {
+			// Specular color
+			material.ks.r = (float)atof(argv[i+1]);
+			material.ks.g = (float)atof(argv[i+2]);
+			material.ks.b = (float)atof(argv[i+3]);
+			i+=4;
+		} else
+		if (strcmp(argv[i], "-sp") == 0) {
+			// Specular power
+			material.sp = (float)atof(argv[i+1]);
+			i+=2;
+		} else
+		if ((strcmp(argv[i], "-pl") == 0) || (strcmp(argv[i], "-dl") == 0)){
+			Light light;
+			// Specular color
+			light.posDir.x = (float)atof(argv[i+1]);
+			light.posDir.y = (float)atof(argv[i+2]);
+			light.posDir.z = (float)atof(argv[i+3]);
+			light.color.r = (float)atof(argv[i+4]);
+			light.color.g = (float)atof(argv[i+5]);
+			light.color.b = (float)atof(argv[i+6]);
+			if (strcmp(argv[i], "-pl") == 0) {
+				// Point
+				light.type = Light::POINT_LIGHT;
+			} else {
+				// Directional
+				light.type = Light::DIRECTIONAL_LIGHT;
+			}
+			lights.push_back(light);
+			i+=7;
+		}
+	}
+}
+
+//****************************************************
+// the usual stuff, nothing exciting here
+//****************************************************
+int main(int argc, char *argv[]) {
+
+	parseArguments(argc, argv);
+
+  	//This initializes glut
+  	glutInit(&argc, argv);
+
+  	//This tells glut to use a double-buffered window with red, green, and blue channels
+  	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+
+  	// Initalize theviewport size
+  	viewport.w = 400;
+  	viewport.h = 400;
+
+  	//The size and position of the window
+  	glutInitWindowSize(viewport.w, viewport.h);
+  	glutInitWindowPosition(0,0);
+  	glutCreateWindow(argv[0]);
+
+   	// Initialize timer variable
+	#ifdef _WIN32
+	lastTime = GetTickCount();
+	#else
+	gettimeofday(&lastTime, NULL);
+	#endif
+
+  	initScene();							// quick function to set up scene
+
+  	glutDisplayFunc(myDisplay);					// function to run when its time to draw something
+  	glutReshapeFunc(myReshape);					// function to run when the window gets resized
+  	glutIdleFunc(myFrameMove);
+
+  	glutMainLoop();							// infinite loop that will keep drawing and resizing and whatever else
+
+  	return 0;
+}
+
+
+
+
+
+
+
+
 
